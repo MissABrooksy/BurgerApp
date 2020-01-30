a7f30d98f987b54a3a202283aa88e104bc4bd64b
Andrea <andreakwilliams1@gmail.com>
2020-01-30 15:13:14 -0500

    comment

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 $("#submit").on('click', function() {
 
-    $("#typedBurgers").val().append($("#addedBurgers"));
+    $("#typedBurgers").val().append($("#addedBurgers")); //want to append as button in "added burgers div"
 })
